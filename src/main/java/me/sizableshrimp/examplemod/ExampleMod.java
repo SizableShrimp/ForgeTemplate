15ec6c316b5c82252cf4a2fe9daa23ae14520b3e
SizableShrimp <sizableshrimp@sizableshrimp.me>
2021-10-05 15:00:55 -0500

    1.12 Template
    Signed-off-by: SizableShrimp <sizableshrimp@sizableshrimp.me>

--- src/main/java/me/sizableshrimp/examplemod/ExampleMod.java
+++ src/main/java/me/sizableshrimp/examplemod/ExampleMod.java
@@ -1,29 +1,25 @@
 package me.sizableshrimp.examplemod;
 
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(ExampleMod.MODID)
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod(modid = ExampleMod.MODID, name = "Example Mod", version = "@VERSION@")
 public class ExampleMod {
     public static final String MODID = "examplemod";
     public static final Logger LOGGER = LogManager.getLogger();
 
     public ExampleMod() {}
 
-    @SubscribeEvent
-    public static void onCommonSetup(FMLCommonSetupEvent event) {
-        // do something when the setup is run on both client and server
-        LOGGER.info("HELLO from common setup!");
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+        LOGGER.info("HELLO from pre init!");
     }
 
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        // do something when the setup is run on only the client
-        LOGGER.info("HELLO from client setup!");
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event) {
+        LOGGER.info("HELLO from init!");
     }
 }
