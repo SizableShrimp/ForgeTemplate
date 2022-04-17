5bef59d9168a0c3a12f356d8f9dac6576e41b2ac
SizableShrimp <sizableshrimp@sizableshrimp.me>
2022-04-16 21:57:28 -0500

    Update Forge and Parchment
    Gradle 7.4.2
    
    Signed-off-by: SizableShrimp <sizableshrimp@sizableshrimp.me>

--- src/main/java/me/sizableshrimp/examplemod/ExampleMod.java
+++ src/main/java/me/sizableshrimp/examplemod/ExampleMod.java
@@ -1,17 +1,17 @@
 package me.sizableshrimp.examplemod;
 
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 @Mod(ExampleMod.MODID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ExampleMod {
     public static final String MODID = "examplemod";
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public ExampleMod() {}
 
